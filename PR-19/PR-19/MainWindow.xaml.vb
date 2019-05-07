c9e98ee08bd03cf9540af8ef1cfef7cef935bcc9
HecklerK <49901822+HecklerK@users.noreply.github.com>
2019-05-07 12:03:32 +0300

    Add files via upload

---
+++ PR-19/PR-19/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Spisok.Items.Add(TB.Text);
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
